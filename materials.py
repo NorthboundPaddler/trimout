661bdcf4f46d104b53d87911ace19f699d9e394e
Aaron Thompson <aaroncthompson1@gmail.com>
2019-03-30 08:36:50 -0500

    Added Window class, and tests for calculating trim lengths for 90 degree joints.

--- materials.py
+++ materials.py
@@ -4,7 +4,7 @@ class RoughOpening(object):
         self.bottom = bottom_width
         self.left = left_height
         self.right = right_height
-        self.sides = setSides()
+        self.sides = self.setSides()
     
     def setSides(self):
         sides = []
@@ -22,3 +22,21 @@ class RoughOpening(object):
     def __str__(self):
         return f"<Rough Opening {self.top}x{self.left}>"
     
+class Window(RoughOpening):
+    def __init__(self, top_width=None, bottom_width=None, left_height=None, right_height=None, jamb_thickness = 0.75, jamb_setback = 0.75):
+        self.jamb_thickness = jamb_thickness
+        self.jamb_setback = jamb_setback
+        RoughOpening.__init__(self, top_width, bottom_width, left_height, right_height)
+   
+    def getAllTrimLengthsFromInsideJambEdge(self, trim_thickness=3, corner_style="90_TOPS_EXTENT"):
+        if   corner_style == '90_TOPS_EXTENT':
+            top_btm_height = self.top - (2*self.jamb_setback) - (2*self.jamb_thickness) + (2*trim_thickness)
+            side_height = self.left - (2*self.jamb_setback) - (2*self.jamb_thickness)
+        
+        return (top_btm_height, side_height)
+    
+    def getTotalTrimInches(self,  trim_thickness=3, corner_style="90_TOPS_EXTENT"):
+        top_btm_inches, sides_inches  = self.getAllTrimLengthsFromInsideJambEdge()
+        return (top_btm_inches*2) + (sides_inches*2)
+
+        
--- test.py
+++ test.py
@@ -1,26 +1,45 @@
 import unittest
 
-from materials import RoughOpening
+from materials import RoughOpening, Window
 
 
 class TestRoughOpening(unittest.TestCase):
-    def __init__(self, methodName='runTest'):
-        self.r_opening4sides = RoughOpening(top_width=48.5, bottom_width=48.5, left_height=60.5, right_height=60.5)
-        self.r_opening3sides = RoughOpening(top_width=36, left_height=82.5, right_height=82.5)
+       
+    def setUp(self):
+        self.rough_opening_4sides =  RoughOpening(top_width=48.5, bottom_width=48.5, left_height=60.5, right_height=60.5)
+        self.rough_opening_3sides =  RoughOpening(top_width=36, left_height=82.5, right_height=82.5)   
         
     def test_4sided_get_total_inches(self):
         """
         Test that it can sum a list of integer properties
         """
-        result = self.r_opening4sides.getTotalInches()
+        result = self.rough_opening_4sides.getTotalInches()
         self.assertEqual(result, 218)
         
     def test_3sided_get_total_inches(self):
         """
         Test that it can sum a list of integer properties
         """
-        result = self.r_opening3sides.getTotalInches()
+        result = self.rough_opening_3sides.getTotalInches()
         self.assertEqual(result, 201)        
 
+class TestWindow(unittest.TestCase):
+    def setUp(self):
+        self.window = Window(top_width=48.5, bottom_width=48.5, left_height=60.5, right_height=60.5)
+    
+    def test_getAllTrimLengthsFromInsideJambEdge(self):
+        '''
+        Test that the correct distance of trim is calculated from inside edge of the jamb 
+        '''
+        result = self.window.getAllTrimLengthsFromInsideJambEdge()
+        self.assertEqual(result, (51.5,57.5))
+    
+    def test_getTotalTrimInches(self):
+        '''
+        Test that the correct inches of trim is calculated
+        '''
+        result = self.window.getTotalTrimInches()
+        self.assertEqual(result, 218)
+        
 if __name__ == '__main__':
     unittest.main()
